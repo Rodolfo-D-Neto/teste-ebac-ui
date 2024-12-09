8b820a02f87583762aa45defe87e55683541d53b
Rodolfo Dutra <rodolfodutraneto2021@gmail.com>
2024-12-09 13:24:27 -0300

    alterações de arquivos

--- cypress/support/page-objects/produtos.page.js
+++ cypress/support/page-objects/produtos.page.js
@@ -18,14 +18,16 @@ class ProdutosPage{
         
     }
 
-    visitarProduto(){
-
-        //codigo
+    visitarProduto(nomeProduto){
+        cy.visit(`produtos/${nomeProduto}` )
     }
 
-    addProdutoCarrinho(){
-
-        //codigo
+    addProdutoCarrinho(tamanho, cor, quantidade){
+        cy.get('.button-variable-item-' + tamanho).click()
+        cy.get('.button-variable-item-' + cor).click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+        
     }
 }
 
