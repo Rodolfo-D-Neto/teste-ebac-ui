3072f63aa317e779760a81704c11cfa5b0463eb7
Rodolfo <rodolfodutraneto2021@gmail.com>
2024-12-27 19:23:20 -0300

    faker e variáveis

--- cypress/e2e/3-loja-ebac/cadastro.cy.js
+++ cypress/e2e/3-loja-ebac/cadastro.cy.js
@@ -18,7 +18,7 @@ describe('Funcionalidade: Cadastro', () => {
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
     });
 
-    it.only('Deve completar o cadastro com sucesso - Usando variáveis', () => {
+    it('Deve completar o cadastro com sucesso - Usando variáveis', () => {
         
         var nome = faker.person.firstName()
         var sobrenome = faker.person.lastName()
